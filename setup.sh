5b8f63b1e69bb1203b0dd093551b2b5dc62c2b1d
Erik de Kamps <erik@luciuswebsystems.nl>
2018-10-10 15:13:08 +0200

    More file creations.

--- setup.sh
+++ setup.sh
@@ -10,3 +10,19 @@ read site_name
 
 # Create the config file.
 touch ./docker/conf/${site_name}.conf
+
+# Create the database file.
+touch ./docker/database/mysqld.cnf
+
+# Create the nginx file.
+touch ./docker/proxy/nginx.conf
+
+# Create the script for adding to hosts file.
+touch ./docker/scripts/add-to-hosts-file.sh
+
+# Create the web files.
+touch ./docker/web/development.services.yml
+touch ./docker/web/php.ini-dev
+touch ./docker/web/settings.dev.php
+touch ./docker/web/settings.live.php
+touch ./docker/web/settings.local.php
